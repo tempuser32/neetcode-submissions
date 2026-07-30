223a1676368ecdc2f13c3cae0a5928d56f204ecd
tempuser32 <22z338@psgtech.ac.in>
2026-07-30 12:28:06 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,29 @@
+"""class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res=0
+        for i in range(len(s)):
+            count={}
+            maxf=0
+            for j in range(i,len(s)):
+                count[s[j]]=1+count.get(s[j],0)
+                maxf=max(maxf,count[s[j]])
+                if (j-i+1)-maxf<=k:
+                    res=max(res,j-i+1)
+        return res
+"""
+class Solution:
+    def characterReplacement(self,s:str,k:int)->int:
+        l=0
+        count={}
+        res=0
+        maxf=0
+        for r in range(len(s)):
+            count[s[r]]=1+count.get(s[r],0)
+            maxf=max(maxf,count[s[r]])
+            while (r-l+1)-maxf>k:
+                count[s[l]]-=1
+                l+=1
+            res=max(res,r-l+1)
+        return res
+
+
